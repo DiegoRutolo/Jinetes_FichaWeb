1b06cf0e231d10bdaf2b5907138c220cedbcbc1f
DiegoRutolo <diegorutolo@gmail.com>
2019-05-06 22:55:05 +0200

    Aspecto general finalizado y sistema de guardado en proceso

--- v1/res/js/main.js
+++ v1/res/js/main.js
@@ -28,6 +28,13 @@ function actualizar() {
 	document.getElementById('mov').value = Math.floor(des/2);
 	document.getElementById('lev').value = Math.floor(fue*2);
 	document.getElementById('hp').value = Math.floor(con*5);
+
+	/* Calcular AC */
+	document.getElementById('acBDES').value = calcBono(des);
+	acArmadura = document.getElementById('acArmadura').value;
+	acModRaza = document.getElementById('acModRaza').value;
+	acExtras = document.getElementById('acExtras').value;
+	document.getElementById('acTotal').value = 15 + calcBono(des) + parseInt(acArmadura) + parseInt(acModRaza) + parseInt(acExtras);
 }
 
 function normalizar(stat) {
@@ -42,4 +49,89 @@ function normalizar(stat) {
 
 function calcBono(stat, bono) {
 	return Math.floor(parseInt(stat)/2 - 20);
+}
+
+function nuevaLinea(lugar) {
+	if (lugar == 'equipo') {
+		document.getElementById('lineasEquipo').innerHTML += '<input type="text" class="lineaTexto">';
+		document.getElementById('lineasPasivas').innerHTML += '<input type="text" class="lineaTexto">';
+	}
+}
+
+function guardar() {
+	//Generales
+	nombre = document.getElementById('iNom').value;
+	raza = document.getElementById('iRaza').value;
+	clase = document.getElementById('iClase').value;
+	edad = document.getElementById('iEdad').value;
+	altura = document.getElementById('iAltur').value;
+	peso = document.getElementById('iPeso').value;
+
+	//principales
+	fue = normalizar(document.getElementById('fue').value);
+	des = normalizar(document.getElementById('des').value);
+	con = normalizar(document.getElementById('con').value);
+	car = normalizar(document.getElementById('car').value);
+	int = normalizar(document.getElementById('int').value);
+	sab = normalizar(document.getElementById('sab').value);
+
+	//Secundario
+	apariencia = document.getElementById('apar').value;
+
+	//Ac
+	acArm = document.getElementById('acArmadura').value;
+	acExtra = document.getElementById('acExtras').value;
+
+	armTexto = document.getElementById('iACnomArmadura').value;
+
+	var pj = {
+		pj: {
+			general: {
+				nombre: document.getElementById('iNom').value,
+				raza: document.getElementById('iRaza').value,
+				clase: document.getElementById('iClase').value,
+				edad: document.getElementById('iEdad').value,
+				altura: document.getElementById('iAltur').value,
+				peso: document.getElementById('iPeso').value
+			},
+			atributos: {
+				fue: fue,
+				des: des,
+				con: con,
+				car: car,
+				int: int,
+				sab: sab,
+				apar: apariencia
+			},
+			ac: {
+				acArm: acArm,
+				acExtra: acExtra
+			},
+			pasivas: [],
+			equipo: []
+		}
+	}
+
+	for (i = 0; i < document.getElementById('lineasPasivas').children.length; i++) {
+		hab = document.getElementById('lineasPasivas').children[i].value;
+		pj['pj']['pasivas'].push(hab);
+	}
+
+	for (i = 0; i < document.getElementById('lineasEquipo').children.length; i++) {
+		hab = document.getElementById('lineasEquipo').children[i].value;
+		pj['pj']['equipo'].push(hab);
+	}
+
+	var jsonPj = JSON.stringify(pj);
+	console.log(jsonPj);
+
+	//download(jsonPj, 'json.txt', 'text/plain');
+}
+
+function download(content, fileName, contentType) {
+    var a = document.createElement("a");
+    var file = new Blob([content], {type: contentType});
+    a.href = URL.createObjectURL(file);
+    a.download = fileName;
+    a.click();
 }
